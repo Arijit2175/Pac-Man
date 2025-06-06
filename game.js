9de29e9c91ed6ee2e81be9fac0b08554fda3c4d0
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-06 09:30:20 +0530

    creating the rectangle sized map

--- game.js
+++ game.js
@@ -1,4 +1,9 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
-
 const pacmanFrames = getElementById("animation");
+const ghostFrames = getElementById("ghosts");
+
+let createRect = (x, y, width, height, color) => {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+}
