436266286239ff3f9e323c5b9402dd44049797b4
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-06 19:36:34 +0530

    creation of ghosts

---
+++ ghost.js
@@ -0,0 +1,15 @@
+class Ghost {
+    constructor(
+        x,
+        y,
+        width,
+        height,
+        speed,
+        imageX,
+        imageY,
+        imageWidth,
+        imageHeight,
+        range
+    ) {
+        
+    }
