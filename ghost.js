369612566c6424bf410b3064520328ad383a6439
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-06 19:39:37 +0530

    applied variables for structure of ghosts

--- ghost.js
+++ ghost.js
@@ -11,5 +11,20 @@ class Ghost {
         imageHeight,
         range
     ) {
-        
+        this.x = x;
+        this.y = y;
+        this.width = wid;
+        this.height = height;
+        this.speed = speed;
+        this.direction = DIRECTION_RIGHT;
+        this.imageX = imageX;
+        this.imageY = imageY;
+        this.imageHeight = imageHeig;
+        this.imageWidth = imageWidth;
+        this.range = range;
+        this.randomTargetIndex = parseInt(Math.rando() * 4);
+        this.target = randomTargetsForGhosts[this.randomTargetIndex];
+        setInterval(() => {
+            this.changeRandomDirection();
+        }, 10000);
     }
