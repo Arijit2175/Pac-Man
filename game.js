b4ed37b9434b1a682a96c718871a798ff2bbcf2c
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-06-06 19:17:31 +0530

    declared ui components

--- game.js
+++ game.js
@@ -7,6 +7,20 @@ let createRect = (x, y, width, height, color) => {
     canvasContext.fillStyle = color;
     canvasContext.fillRect(x, y, width, height);
 }
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
+let lives = 3;
+let ghostCount = 4;
+let ghostImageLocations = [
+    { x: 0, y: 0 },
+    { x: 176, y: 0 },
+    { x: 0, y: 121 },
+    { x: 176, y: 121 },
+];
+
 let fps = 30;
 let oneBlockSize = 20;
 let wallColor = "#342dca";
